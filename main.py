357372ed7ee89a56380b89b608282d4eb759e996
Danilo Fernandes <danilofernandes10a@gmail.com>
2026-09-12 17:09:51 -0300

    Atualização

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ db_host = os.getenv("DB_HOST")
 db_port = os.getenv("DB_PORT")
 
 
-url = "https://economia.awesomeapi.com.br/last/USD-BRL"
+url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
 
 conn = psycopg.connect(
     dbname = db_name,
@@ -75,3 +75,25 @@ except requests.exceptions.RequestException as E:
 finally:
     cur.close()
     conn.close()
+
+
+def extrair_cotacoes():
+    url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
+    cotacoes = []
+    try:
+        resposta = requests.get(url)
+        if resposta.status_code == 200:
+                dados = resposta.json()
+                for item in dados.values():
+                        moeda = f"{item['code']}-{item['codein']}"
+                        valor = float(item['bid'])
+                        data_hora = item(['create_date'])
+                        cotacoes.append((moeda, valor, data_hora))
+        else:
+                print(
+                    f"Erro na requisição: {resposta.status_code}"
+                )
+    except Exception as e:
+         print(f"Erro na requisição: {e}")
+    return cotacoes
+
